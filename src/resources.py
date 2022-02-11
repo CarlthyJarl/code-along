f7d5e4b44bd8d8f524c1df287e0dc5dbda2ede9c
carl <carl.samme@elev.ga.ntig.se>
2022-02-11 12:56:11 +0100

    Added Character class and exampels

---
+++ src/resources.py
@@ -0,0 +1,23 @@
+# imports
+# global variabels
+# classes
+class Character:
+
+    def __init__(self, name, health, damage, armor):
+        self.name = name
+        self.health = health
+        self.damage = damage 
+        self.armor = armor
+
+        def __str__(self):
+            return F"Name: {self.name}\nHealth: {self.helth}\nDamage: {self.damage}\nArmor: {self.armor}"
+
+# functions
+def hi():
+    print("hi")
+
+# main code
+
+
+
+
