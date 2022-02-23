9f7999902f1a4d4734aaea2b10ae931b81621b9e
carl <carl.samme@elev.ga.ntig.se>
2022-02-23 09:42:53 +0100

    gjorde grejer

--- src/main.py
+++ src/main.py
@@ -1,81 +1,76 @@
-from resources import Character, Goblin, save_character, load_characters
+from resources import Character, Goblin, save_character, load_characters, create_character
 import random
 
 def fight(fighter : Character, enemies : list):
-
+    
     while not len(enemies) == 0:
         fighter_target = random.choice(enemies)
         fighter_target.take_damage(fighter.attack())
         if fighter_target.get_health() == 0:
-            print("Target Destroyed")
+            print("Target has died!")
             enemies.remove(fighter_target)
-            if len(enemies) == 0:
-                break
-
-    print(F"Fight is over! {fighter.name} won")
-
-
+            if len(enemies) == 0: break
+    
+    print(f"Fight is over! {fighter.name} won!")
+    
 def new_fight(players: list, enemies: list):
-    participants = players + enemies # Slår ihop alla deltagare till en lista
+    participants = players + enemies # Slå ihop alla deltagare till en lista
     random.shuffle(participants)
-
-    for char in participants: #Check if goblin or character
-        target=""
+    
+    for char in participants:
+        target = ""
+        # Check if goblin or character
         if char in players:
             target = random.choice(enemies)
         else:
             target = random.choice(players)
-
+        
         target.take_damage(char.attack())
         if target.get_health() == 0:
-            print(F"{char.get_name()} has killed {target.get_name()}.")
+            print(f"{char.get_name()} has killed {target.get_name()}.")
             if(type(target) == Goblin):
                 enemies.remove(target)
-            else:   
+            else:
                 players.remove(target)
             participants.remove(target)
         else:
-            print(F"{target.get_name()} was attacked by {char.get_name()}.")
-            print(F"{target.get_name()} has {target.get_health()} healthpoints left")
+            print(f"{target.get_name()} was attacked by {char.get_name()}.")
+            print(f"{target.get_name()} has {target.get_health()} healthpoints left.")
         if len(enemies) == 0 or len(players) == 0: break
-
-        
+    
+    
 
 def main():
     enemies = []
-    players = []
-
-    nick = Character("Nick", 15, 3, 1)
-    emy = Character("Emy", 20, 6, 5)
-    players.append(nick)
-    players.append(emy)
-
-    print(nick)
-    print()
-    print(emy)
-
-    enemies.append(Goblin(1))
-    print("\nGoblin")
-    print(enemies[0])
-
-    #fight(emy, enemies)
-
+    players = load_characters()
+    
+    print("Would you like to create a new character? (y/n)")
+    new_char = input(": ")
+    if new_char.lower() == "y":
+        new = create_character()
+        players.append(new)
+    
+    amount_of_goblins = int(input("How many goblins should they fight?: "))
+    for i in range(amount_of_goblins):
+        enemies.append(Goblin(i+1))
+    
     while len(enemies) != 0 and len(players) != 0:
         new_fight(players, enemies)
     if len(enemies) == 0:
         print("The players won!")
+        print("Would you like to save the remaining characters? (y/n)")
+        while True:
+            save_progress = input(": ")
+            if save_progress.lower() == "y":
+                save_character(players)
+                break
+            elif save_progress.lower() == "n":
+                break
+            else:
+                print("That was not a valid option.")
     elif len(players) == 0:
         print("The Goblins won!")
-
-    #save_character(emy)
-
-    players = load_characters()
-    for player in players:
-        print(player)
-
     
- 
-if __name__ == "__main__":
+    
+if __name__=="__main__":
     main()
-
-
--- src/resources.py
+++ src/resources.py
@@ -1,105 +1,127 @@
 # imports
-# global variabels
+from random import randint, choice
+# global variables
 # classes
-class Character:
+class Weapon:
+    def __init__(self, name, damage):
+        self.name = name
+        self.damage = damage
+    
+    def return_damage(self):
+        return self.damage
 
+class Character:
+    
     def __init__(self, name, health, damage, armor):
         self.name = name
         self.health = health
-        self.damage = damage 
+        self.damage = damage
         self.armor = armor
-
+    
     def __str__(self):
-        return F"Name: {self.name}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}"
+        return f'Name: {self.name}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}'
 
     def take_damage(self, dmg):
         actual_damage = dmg - self.armor
-        if actual_damage < 0: actual_damage = 0 
-        if (self.health - actual_damage) < 0:
-            self.health = 0
-        else:
-            self.health -= actual_damage
-
+        if actual_damage < 0: actual_damage = 0
+        if (self.health - actual_damage) < 0: self.health = 0
+        else: self.health -= actual_damage
+    
     def attack(self):
         return self.damage
 
     def get_health(self):
         return self.health
-
+    
     def get_name(self):
         return self.name
-
+    
     def get_all_attributes(self):
         return self.name, self.health, self.damage, self.armor
-
     
-
 class Goblin:
-
+    
     def __init__(self, id):
         self.id = id
         self.health = 10
-        self.damage = 3
         self.armor = 2
-
+        self.give_weapon()
+        self.damage = self.weapon.return_damage()
+        
     def __str__(self):
-        return F"Name: {self.id}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}"
+        return f'ID: {self.id}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}'
 
+    def give_weapon(self):
+        weapons = []
+        weapons.append(Weapon("Rusty Spear", 3))
+        weapons.append(Weapon("Rusty Cleaver", 2))
+        weapons.append(Weapon("Stone Axe", 1))
+        self.weapon = choice(weapons)
+    
     def take_damage(self, dmg):
         actual_damage = dmg - self.armor
-        if actual_damage < 0: actual_damage = 0 
-        if (self.health - actual_damage) < 0:
-            self.health = 0
-        else:
-            self.health -= actual_damage
-    
+        if actual_damage < 0: actual_damage = 0
+        if (self.health - actual_damage) < 0: self.health = 0
+        else: self.health -= actual_damage
     
     def attack(self):
         return self.damage
-
+    
     def get_health(self):
         return self.health
-
+    
     def get_name(self):
         return f"Goblin #{self.id}"
 
+
+        
 # functions
-def hi():
-    print("hi")
-
-
-
-def save_character(character: Character):
-    """
-    Tar in karaktär, bryter ner dess attribut och sparar ner på fil
+def hello():
+    print("Hello World")
     
-    Args:
-        character (Character): Det objekt som ska sparas ner påå fil
+def save_character(characters : list()):
     """
-    name, health, damage, armor = character.get_all_attributes()
-    with open("character_file.txt", "w", encoding="utf8") as f:
+    Tar in karaktär, bryter ner dess attribut och sparar ner på fil.
+    Args:
+        character (Character): Det objekt som ska sparas ner på fil.
+    """
+    saved_characters = []
+    for character in characters:
+        name, health, damage, armor = character.get_all_attributes()
         save_string = f"{name}/{health}/{damage}/{armor}\n"
-        f.write(save_string)
-        print(F"{name} has been successfully saved. ")
-
+        saved_characters.append(save_string)
+        
+    with open("character_file.txt", "w", encoding="utf8") as f:
+        for char in saved_characters:
+            f.write(char)
+        print(f"Characters has been successfully saved.")
 
 def load_characters():
-
+    
     with open("character_file.txt", "r", encoding="utf8") as f:
         characters = []
         for line in f.readlines():
             attributes = line.split("/")
-            this_char = Character(attributes[0],
+            this_char = Character(attributes[0], 
                                   int(attributes[1]),
                                   int(attributes[2]),
                                   int(attributes[3]))
             characters.append(this_char)
-        print("Characters has been loaded from file.")
-        return characters
+    print("Characters has been loaded from file.")
+    return characters
 
-
+def create_character():
+    # tänk på name, hp, damage, armor
+    print("Welcome to the character creation!")
+    print("What is your character called?")
+    name = input("Name: ")
+    health = randint(10, 25)
+    damage = randint(1, 6)
+    armor = randint(0, 5)
+    
+    return_char = Character(name, health, damage, armor)
+    print("You have created the following character.")
+    print(return_char)
+    return return_char
+    
 # main code
-
-
-
-
